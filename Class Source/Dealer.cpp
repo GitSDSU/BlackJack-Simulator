b64396fe20d5bb125c3a407018dae835ff43f609
GitSDSU <lurzua.sdsu@gmail.com>
2019-07-21 13:33:01 -0700

    Dealer

--- Class Source/Dealer.cpp
+++ Class Source/Dealer.cpp
@@ -1,4 +1,5 @@
 #include "..\Class Header\Dealer.h"
+#include "..\Class Header\Card.h"
 
 Dealer::Dealer()
 {
@@ -7,5 +8,9 @@ Dealer::Dealer()
 
 Dealer::~Dealer()
 {
-
+    for (auto it = hand.begin(); it != hand.end(); ++it)
+    {
+        delete (*it);
+    }
+    hand.clear();
 }
